7922a7a7c3306b1b952d18d2438c3b0d7de511e1
pramodaya <pramodayajayalath@gmail.com>
2023-06-13 23:23:14 +0200

    Update Test01_Get.java

--- src/test/java/org/example/api/Test01_Get.java
+++ src/test/java/org/example/api/Test01_Get.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.testng.Assert.*;
 
 public class Test01_Get {
 
@@ -13,7 +14,12 @@ public class Test01_Get {
     public void test1() {
 
         Response response = RestAssured.given().get("https://reqres.in/api/users?page=2");
-        response.then().statusCode(200).body("data.id[0]", equalTo(7));
+        response
+                .then()
+                .statusCode(200)
+                .body("data.id[0]", equalTo(7));
+
+        assertEquals(response.statusCode(), 200);
 
     }
 }
